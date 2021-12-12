51b0c633ae2f0c02dd4ecc501ed95bcb3ebaf196
windowdotonload <275981471@qq.com>
2021-12-12 17:11:53 +0800

    mod

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: windowdotonload
+ */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <DndProvider backend={HTML5Backend}>
     <App />
-  </React.StrictMode>,
+  </DndProvider>,
   document.getElementById('root')
 )
